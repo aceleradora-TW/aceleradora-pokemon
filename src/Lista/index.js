4b87d438e07ba6dd242adfaccd647ee9d24a6ba8
Avanilson Santos <avanilsonsantos@Avanilsons-MacBook-Pro.local>
2021-04-30 16:16:24 -0300

    React - Sessão 3

--- src/Lista/index.js
+++ src/Lista/index.js
@@ -1,12 +1,15 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function Lista(props){
     const {alunas} = props
 
     const [name, setName] = useState('')
+    const [pokemons, setPokemons] = useState([])
 
     useEffect(()=>{
         console.log('Chegou aqui')
+        getAllPokeomns()
     },[])
 
     useEffect(()=>{
@@ -17,26 +20,45 @@ export default function Lista(props){
         return alert('Clicou no botão')
     }
 
-    function testeEvento(nameEvent){
-        // console.log(nameEvent)
-        setName(nameEvent)
+    async function getAllPokeomns(){
+        const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
+        
+        data.results.forEach(async (result)=>{
+            const img = await getDetails(result.url)
+            console.log(img)
+            pokemons.push(
+                {name:result.name, image:img}
+            )
+            setPokemons([...pokemons])
+        })
     }
 
+    async function getDetails(url){
+        const {data} = await axios.get(url)
+        return data.sprites.front_default
+    }
+
+
+    function testeEvento(event){
+        // console.log(event.target.value)
+        setName(event.target.name)
+    }
+    
     return (
         <>
             <ul>
-                {alunas.map(aluna=>{
+                {pokemons.map(pokemon=>{
                     return (
-                        <li key={Math.random()}>{aluna}</li>
+                        <li key={Math.random()}>{pokemon.name}
+                        <img src={pokemon.image} alt=''/>'
+                        </li>
                     )
                 })}
             </ul>
             <button onClick={()=>testeDeClique()}>Clicar</button>
             <p/>
-            Nome: <input type='text' onChange={(event)=>testeEvento(event.target.value)} />
-
+                Nome: <input type='text' onChange={(event)=>testeEvento(event)} />
             {name}
-
         </>
     )
 }
